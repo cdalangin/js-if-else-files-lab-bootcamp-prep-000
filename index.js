0b01c5223200377c81a97386158092620bf46333
Cirill Florenz A Dalangin <florenzdalangin@gmail.com>
2018-07-09 18:51:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-var name = "Joe"
+var name = "Susan"
 var height = parseInt(70)
 if (name==="Susan") {
   console.log("The name is correct")
-} else {
-  console.log("Expected the name to be Susan")
-}
+} 
 
 // Don't worry about this
 module.exports = { name, height
